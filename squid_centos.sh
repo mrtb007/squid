5aa9922ab2ca71188e19087865bf412046633406
mrtb007 <84973536+mrtb007@users.noreply.github.com>
2021-05-29 15:37:18 +0700

    Update squid_centos.sh

--- squid_centos.sh
+++ squid_centos.sh
@@ -8,8 +8,8 @@
 # Site: https://knaved.com
 # Version 0.1
 
-squid_user=
-squid_password=
+squid_user=mrtb007
+squid_password=q3.-jrW645!Q+89f
 
 yum -y install squid httpd-tools
 
@@ -17,9 +17,9 @@ htpasswd -b -c /etc/squid/passwd $squid_user $squid_password
 
 mv /etc/squid/squid.conf /etc/squid/squid.conf.bak
 touch /etc/squid/blacklist.acl
-wget -O /etc/squid/squid.conf  https://raw.githubusercontent.com/khaledalhashem/squid/master/squid_centos.conf
+wget -O /etc/squid/squid.conf  https://raw.githubusercontent.com/mrtb007/squid/main/squid_centos.conf
 
-iptables -I INPUT -p tcp --dport 3128 -j ACCEPT
+iptables -I INPUT -p tcp --dport 8668 -j ACCEPT
 #/sbin/iptables-save
 /sbin/service iptables save
 
