0384a6cd415e64b2c282416cbed1a7f4470da95c
mrtb007 <84973536+mrtb007@users.noreply.github.com>
2021-05-29 15:29:56 +0700

    Create centos.sh

---
+++ centos.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# <UDF name="squid_user" Label="Proxy Username" />
+# <UDF name="squid_password" Label="Proxy Password" />
+# Squid Proxy Server
+# Author: admin@hostonnet.com
+# Blog: https://blog.hostonnet.com
+# Edits: Khaled AlHashem
+# Site: https://knaved.com
+# Version 0.1
+
+squid_user=mrtb007
+squid_password=Thikdsfds^%^
+
+yum -y install squid httpd-tools
+
+htpasswd -b -c /etc/squid/passwd $squid_user $squid_password
+
+mv /etc/squid/squid.conf /etc/squid/squid.conf.bak
+touch /etc/squid/blacklist.acl
+wget -O /etc/squid/squid.conf  https://raw.githubusercontent.com/khaledalhashem/squid/master/squid_centos.conf
+
+iptables -I INPUT -p tcp --dport 3128 -j ACCEPT
+#/sbin/iptables-save
+/sbin/service iptables save
+
+systemctl restart squid
+systemctl enable squid
+systemctl status squid
+#update-rc.d squid defaults
